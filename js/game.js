d6ccae4552651c3cb85a13e500145fd78807d117
Rainald Balog <rainald.balog@arobs.com>
2020-01-13 04:32:55 -0800

    added game difficulty levels

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+const body = document.getElementsByTagName("body")[0];
 const gameBoard = document.getElementsByClassName("gameBoard")[0];
 const restart = document.getElementsByClassName("restart")[0];
 const actualScore = document.getElementById("actualscore");
@@ -12,185 +13,38 @@ const tadaSound = document.getElementById("tada");
 const stopperModal = document.getElementById("stopperModal");
 const movesModal = document.getElementById("movesModal");
 
-const level = 8;
+const ranger = document.getElementById("levelranger");
+const rangespan = document.getElementById("rangevalue");
+let level = ranger.value;
+rangespan.textContent = level;
 
 const createCardList = () => {
-    let icons = faIcons.slice(0, faIcons.length - (faIcons.length - level));
-    icons = [...icons, ...icons]; // duplicate
-    let cards = icons.map(value => {
-        let li = document.createElement("li");
-        li.classList.add("card");
-        let item = document.createElement("i");
-        item.classList.add(faClass);
-        item.classList.add(value);
-        li.appendChild(item);
-        return new Card(li, false);
-    });
-    return cards;
+  let icons = faIcons.slice(0, faIcons.length - (faIcons.length - level));
+  icons = [...icons, ...icons]; // duplicate
+  let cards = icons.map(value => {
+    let li = document.createElement("li");
+    li.classList.add("card");
+    let item = document.createElement("i");
+    item.classList.add(faClass);
+    item.classList.add(value);
+    li.appendChild(item);
+    return new Card(li, false);
+  });
+  return cards;
 };
-
-class Game {
-    constructor(cards) {
-        this.myInterval = null;
-        this.mySetTimeout = null;
-        this.cards = cards;
-        this.init();
-    }
-
-    clearAllCardsClasses() {
-        this.cards.forEach(card => {
-            card.cardObj.classList.remove("matched");
-            card.cardObj.classList.remove("unmatched");
-        });
-    }
-
-    init = () => {
-        clearTimeout(this.mySetTimeout);
-        this.clearAllCardsClasses();
-        this.twoCard = [];
-        this.clickCounter = 0;
-        this.timerTime = 0;
-        this.firstClick = false;
-        stopper.textContent = "00:00:00";
-        stopperModal.textContent = "00:00:00";
-    };
-
-    start = () => {
-        this.cards = shuffle(this.cards);
-        this.addToGameBoard();
-        this.setClickable(this.cards);
-    };
-
-    restart = () => {
-        this.init();
-        document.getElementsByTagName("ul")[0].remove();
-        this.cards.forEach(element => {
-            element.setVisible(false);
-        });
-        this.start();
-        moves.textContent = 0;
-        movesModal.textContent = 0;
-        clearInterval(this.myInterval);
-    };
-
-    isFinished = () => {
-        return this.cards.every(card => card.isVisible());
-    };
-
-    startStopper = () => {
-        this.myInterval = setInterval(() => {
-            this.timerTime++;
-            let numOfHours = Math.floor(this.timerTime / 3600);
-            let numOfMins = Math.floor((this.timerTime % 3600) / 60);
-            let numOfSecs = Math.floor((this.timerTime % 3600) % 60);
-            if (numOfHours < 10) numOfHours = "0" + numOfHours;
-            if (numOfSecs < 10) numOfSecs = "0" + numOfSecs;
-            if (numOfMins < 10) numOfMins = "0" + numOfMins;
-            stopperModal.textContent = stopper.textContent =
-                numOfHours + ":" + numOfMins + ":" + numOfSecs;
-        }, 1000);
-    };
-
-    addToGameBoard = () => {
-        let ul = document.createElement("ul");
-        this.cards.forEach(card => {
-            ul.appendChild(card.cardObj);
-        });
-        gameBoard.appendChild(ul);
-    };
-
-    setClickable = cards => {
-        cards.forEach(card => {
-            card.cardObj.onclick = () =>
-                this.onClickHandelForCards(card, this.timeoutCallback); // callback for settimeout
-        });
-    };
-
-    // https://gamedev.stackexchange.com/questions/43962/how-to-devise-a-scoring-algorithm-based-on-elapsed-time-and-number-of-moves
-    getScore = () => {
-        const minimumMoves = this.cards.length;
-        const minimumTime = this.cards.length;
-        const score =
-            (minimumTime / this.timerTime) *
-            (minimumMoves / this.clickCounter) *
-            (minimumMoves * minimumTime * level); // harono
-        return score;
-    };
-
-    /**
-     * @param {string} command         Now only 'add' or 'remove'.
-     * @param {string} className       A CSS class name.
-     */
-    manageStyleClassToCards(command, classname) {
-        if (command === "remove") {
-            this.twoCard[0].cardObj.classList.remove(classname);
-            this.twoCard[1].cardObj.classList.remove(classname);
-        } else if (command === "add") {
-            this.twoCard[0].cardObj.classList.add(classname);
-            this.twoCard[1].cardObj.classList.add(classname);
-        } else {
-            new Error("Command '" + command + "' is not implemeted yet!");
-        }
-    }
-
-    timeoutCallback = () => {
-        this.setClickable(this.twoCard);
-        this.twoCard[0].setVisible(false);
-        this.twoCard[1].setVisible(false);
-        this.manageStyleClassToCards("remove", "unmatched");
-        this.twoCard = [];
-    };
-
-    onClickHandelForCards = (card, timeoutCallback) => {
-        if (!this.firstClick) {
-            this.startStopper();
-            this.firstClick = true;
-        }
-
-        if (this.twoCard.length < 2) {
-            this.twoCard.push(card);
-            card.setVisible(true);
-            tapSound.play();
-
-            card.cardObj.onclick = null; // disable onclick event until check eqaualiti twoCards
-
-            movesModal.textContent = moves.textContent = ++this.clickCounter;
-            if (this.isFinished()) {
-                clearInterval(this.myInterval);
-                actualscore.textContent = this.getScore().toFixed(2);
-
-                // Open nav when finished game ---- from ./modal.js
-                toggleClasses();
-                showModal();
-                showModalChildren();
-                tadaSound.play();
-            }
-
-            if (
-                this.twoCard.length === 2 &&
-                !Card.isEqual(this.twoCard[0], this.twoCard[1])
-            ) {
-                this.manageStyleClassToCards("add", "unmatched");
-                this.mySetTimeout = setTimeout(() => {
-                    timeoutCallback();
-                }, 2000);
-            } else if (
-                this.twoCard.length === 2 &&
-                Card.isEqual(this.twoCard[0], this.twoCard[1])
-            ) {
-                this.manageStyleClassToCards("add", "matched");
-                successSound.currentTime = 0;
-                successSound.play();
-                this.twoCard = [];
-            }
-        }
-    };
-}
 
 let cards = createCardList();
 let game = new Game(cards);
 game.start();
 
+ranger.addEventListener("input", () => {
+  level = ranger.value;
+  rangespan.textContent = level;
+  cards = createCardList();
+  game.cards = cards;
+  game.restart();
+});
+
 restart.addEventListener("click", () => {
-    game.restart();
+  game.restart();
 });
